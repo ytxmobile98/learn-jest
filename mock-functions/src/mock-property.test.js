68a4c3a85023b172330ab59af340f2907afcfe62
ytxmobile98 <5900105+ytxmobile98@users.noreply.github.com>
2024-05-15 14:40:12 +0800

    test: mock members

--- mock-functions/src/mock-property.test.js
+++ mock-functions/src/mock-property.test.js
@@ -17,3 +17,46 @@ describe('mock property', () => {
         expect(myMock2.mock.contexts).toContain(b);
     });
 });
+
+describe('mock members', () => {
+    test('mock members 1', () => {
+        const someMockFunction = jest.fn((a, b) => 'return value');
+        const element = undefined;
+
+        someMockFunction('first arg', 'second arg');
+
+        // The function was called exactly once
+        expect(someMockFunction.mock.calls).toHaveLength(1);
+
+        // The first arg of the first call to the function was 'first arg'
+        expect(someMockFunction.mock.calls[0][0]).toBe('first arg');
+
+        // The second arg of the first call to the function was 'second arg'
+        expect(someMockFunction.mock.calls[0][1]).toBe('second arg');
+
+        // The return value of the first call to the function was 'return value'
+        expect(someMockFunction.mock.results[0].value).toBe('return value');
+
+        // The function was called with a certain `this` context: the `element` object.
+        expect(someMockFunction.mock.contexts[0]).toBe(element);
+    });
+
+    test('mock members 2', () => {
+        const someMockFunction = jest.fn(function() {
+            this.name = 'test';
+        });
+
+        new someMockFunction();
+        new someMockFunction('test');
+
+        // This function was instantiated exactly twice
+        expect(someMockFunction.mock.instances.length).toBe(2);
+
+        // The object returned by the first instantiation of this function
+        // had a `name` property whose value was set to 'test'
+        expect(someMockFunction.mock.instances[0].name).toBe('test');
+
+        // The first argument of the last call to the function was 'test'
+        expect(someMockFunction.mock.lastCall[0]).toBe('test');
+    });
+});
