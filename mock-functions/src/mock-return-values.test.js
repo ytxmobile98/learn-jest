c2dd0053b44121bf7af623169b4e879e50a9556b
ytxmobile98 <5900105+ytxmobile98@users.noreply.github.com>
2024-05-15 19:05:06 +0800

    test: continuation passing

--- mock-functions/src/mock-return-values.test.js
+++ mock-functions/src/mock-return-values.test.js
@@ -6,3 +6,18 @@ test('mock return values', () => {
 
     expect([myMock(), myMock(), myMock(), myMock()]).toEqual([10, 'x', true, true]);
 });
+
+test('continuation passing', () => {
+    const filterTestFn = jest.fn();
+
+    // Make the mock return `true` for the first call,
+    // and `false` for the second call
+    filterTestFn.mockReturnValueOnce(true).mockReturnValueOnce(false);
+
+    const result = [11, 12].filter(num => filterTestFn(num));
+
+    expect(result).toEqual([11]);
+
+    expect(filterTestFn.mock.calls[0][0]).toBe(11);
+    expect(filterTestFn.mock.calls[1][0]).toBe(12);
+});
