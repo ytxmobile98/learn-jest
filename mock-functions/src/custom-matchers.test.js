a16b1623304926abeac0941815e43177a5c55b85
ytxmobile98 <5900105+ytxmobile98@users.noreply.github.com>
2024-05-16 23:07:00 +0800

    test: custom matchers (inspecting the `.mock` property)

--- mock-functions/src/custom-matchers.test.js
+++ mock-functions/src/custom-matchers.test.js
@@ -25,3 +25,43 @@ describe('custom matchers', () => {
         expect(mockFunc).toMatchSnapshot();
     });
 });
+
+describe('custom matchers (inspecting the `.mock` property)', () => {
+    const mockFunc = jest.fn();
+
+    const arg1 = 'arg1';
+    const arg2 = 'arg2';
+
+    test('called at least once', () => {
+        mockFunc();
+        expect(mockFunc.mock.calls.length).toBeGreaterThan(0);
+    });
+
+    test('called with specific arguments', () => {
+        mockFunc(arg1, arg2);
+        expect(mockFunc.mock.calls).toContainEqual([arg1, arg2]);
+    });
+
+    test('the last call was called with specific arguments', () => {
+        mockFunc();
+        mockFunc(arg1, arg2);
+        expect(mockFunc.mock.calls[mockFunc.mock.calls.length - 1]).toEqual([arg1, arg2]);
+    });
+
+    test('first arg of last call should be 42', () => {
+        mockFunc(42, 43);
+        expect(mockFunc.mock.calls[mockFunc.mock.calls.length - 1][0]).toBe(42);
+    });
+
+    test('check mock snapshot', () => {
+        // A snapshot will check that a mock was invoked the same number of times,
+        // in the same order, with the same arguments. It will also assert on the name.
+
+        const mockFunc = jest.fn().mockName('a mock name');
+
+        mockFunc(arg1, arg2);
+
+        expect(mockFunc.mock.calls).toEqual([[arg1, arg2]]);
+        expect(mockFunc.getMockName()).toBe('a mock name');
+    });
+});
